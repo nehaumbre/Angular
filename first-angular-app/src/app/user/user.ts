d7acf29dacfe8af6706686e1f2fbaef79e56ba13
nehaumbre <nehaumbre28@gmail.com>
2026-06-30 15:01:36 +0530

    revert to code without using signals
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

--- first-angular-app/src/app/user/user.ts
+++ first-angular-app/src/app/user/user.ts
@@ -10,17 +10,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  // selectedUser = this.getRandomUser();
-  selectedUser = signal(this.getRandomUser()); // using signal to make selectedUser reactive
+  selectedUser = this.getRandomUser();
+  // selectedUser = signal(this.getRandomUser()); // using signal to make selectedUser reactive
 
-  // get imagePath() {
-  //   // return 'assets/users/' + this.selectedUser.avatar;
-  //   return 'assets/users/' + this.selectedUser().avatar; //using signals
-  // }
+  get imagePath() {
+    // return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.selectedUser.avatar; //using signals
+  }
 
-  imagePath = computed(() => 
-    'assets/users/' + this.selectedUser().avatar
-  ); //!using signals and computed values
+  // imagePath = computed(() => 
+  //   'assets/users/' + this.selectedUser().avatar
+  // ); //!using signals and computed values
 
   private getRandomUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -30,9 +30,9 @@ export class User {
   onSelectedUser() {
     // console.log("clicked!")
     // this.selectedUser = DUMMY_USERS[Math.floor(Math.random()* DUMMY_USERS.length)];
-    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(this.getRandomUser());
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(this.getRandomUser());
     // * using signals to update the value of selectedUser use .set
-    // this.selectedUser = this.getRandomUser();
+    this.selectedUser = this.getRandomUser();
   }
 }
