745ed0ebccecc2a2823403e6c4fe9a1f2e6f0d61
nehaumbre <nehaumbre28@gmail.com>
2026-07-06 01:11:56 +0530

    Creating and Conditionally Rendering Another Component: Feature: Add new task when clicked on Add Task button
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

--- first-angular-app/src/app/tasks/tasks.ts
+++ first-angular-app/src/app/tasks/tasks.ts
@@ -1,15 +1,17 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import {Task} from './task/task';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks {
   @Input({ required: true }) userId!: string | undefined;
   @Input({ required: true }) name!: string | undefined;
+  isAddingTask = false;
 
   dummyTasks = [
     {
@@ -48,5 +50,9 @@ export class Tasks {
 
   onTaskComplete(id:string){
     this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
   }
 }
