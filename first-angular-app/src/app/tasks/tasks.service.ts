e2f1255ea90b848e0252fa30352e28f3bfe79161
nehaumbre <nehaumbre28@gmail.com>
2026-07-08 12:03:17 +0530

    Getting started with services: transfering alll the task m anagement logic to the task.service.ts file instead of tasks.ts
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

---
+++ first-angular-app/src/app/tasks/tasks.service.ts
@@ -0,0 +1,54 @@
+// ! A service is a class that provides a specific functionality to the application.
+// ! It can be used to share data and logic across multiple components.
+// ! In this case, the TasksService is responsible for managing the list of tasks in the application.
+import { type NewTaskData } from "./task/task.model";
+class TasksService {
+  private dummyTasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u7',
+      title: 'Prepare issue template',
+      summary: 'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+    {
+      id: 't4',
+      userId: 'u1',
+      title: 'Make Changes',
+      summary: 'Make the necessary changes to the application.',
+      dueDate: '2025-12-31',
+    },
+  ];
+
+  getUserTasks(userId: string) {
+    return this.dummyTasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(userId: string, taskData: NewTaskData) {
+    this.dummyTasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+    });
+  }
+
+  removeTask(id: string) {
+    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  }
+}
--- first-angular-app/src/app/tasks/tasks.ts
+++ first-angular-app/src/app/tasks/tasks.ts
@@ -14,44 +14,44 @@ export class Tasks {
   @Input({ required: true }) name!: string ;
   isAddingTask = false;
 
-  dummyTasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u7',
-      title: 'Prepare issue template',
-      summary: 'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-    {
-      id: 't4',
-      userId: 'u1',
-      title: 'Make Changes',
-      summary: 'Make the necessary changes to the application.',
-      dueDate: '2025-12-31',
-    },
-  ];
+  // dummyTasks = [
+  //   {
+  //     id: 't1',
+  //     userId: 'u1',
+  //     title: 'Master Angular',
+  //     summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+  //     dueDate: '2025-12-31',
+  //   },
+  //   {
+  //     id: 't2',
+  //     userId: 'u3',
+  //     title: 'Build first prototype',
+  //     summary: 'Build a first prototype of the online shop website',
+  //     dueDate: '2024-05-31',
+  //   },
+  //   {
+  //     id: 't3',
+  //     userId: 'u7',
+  //     title: 'Prepare issue template',
+  //     summary: 'Prepare and describe an issue template which will help with project management',
+  //     dueDate: '2024-06-15',
+  //   },
+  //   {
+  //     id: 't4',
+  //     userId: 'u1',
+  //     title: 'Make Changes',
+  //     summary: 'Make the necessary changes to the application.',
+  //     dueDate: '2025-12-31',
+  //   },
+  // ];
 
-  get selectedUserTasks() {
-    return this.dummyTasks.filter((task) => task.userId === this.userId);
-  }
+  // get selectedUserTasks() {
+  //   return this.dummyTasks.filter((task) => task.userId === this.userId);
+  // }
 
-  onTaskComplete(id:string){
-    this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
-  }
+  // onTaskComplete(id:string){
+  //   this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id);
+  // }
 
   onStartAddTask(){
     this.isAddingTask = true;
@@ -61,14 +61,14 @@ export class Tasks {
     this.isAddingTask = false;
   }
 
-  onAddTask(taskData: NewTaskData){
-    this.dummyTasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.dueDate,
-  })
-    this.isAddingTask = false; // to close the dailog
-  }
+  // onAddTask(taskData: NewTaskData){
+  //   this.dummyTasks.unshift({
+  //     id: new Date().getTime().toString(),
+  //     userId: this.userId,
+  //     title: taskData.title,
+  //     summary: taskData.summary,
+  //     dueDate: taskData.dueDate,
+  // })
+  //   this.isAddingTask = false; // to close the dailog
+  // }
 }
